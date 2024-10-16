d328e62382ffb16f4866fc133372143312cf0055
Yasin Rabiee <hlww6303@gmail.com>
2024-10-16 13:13:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector(`#canvas`);
 const ctx = canvas.getContext(`2d`);
+
 ctx.moveTo(150, 50);
 ctx.lineTo(100, 75);
 ctx.lineTo(200, 75);
@@ -8,6 +9,7 @@ ctx.moveTo(100, 75);
 ctx.lineTo(100, 200);
 ctx.moveTo(200, 75);
 ctx.lineTo(200, 200);
+
 ctx.strokeStyle = `red`;
 ctx.lineWidth = 2;
 ctx.lineCap = `round`;
